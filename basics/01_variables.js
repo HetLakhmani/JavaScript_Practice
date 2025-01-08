88456ba297f87cda2a76def3f163ec3607b39f21
Het Lakhmani <121217603+HetLakhmani@users.noreply.github.com>
2025-01-08 10:39:49 +0000

    variables and datatypes

---
+++ basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 232314
+let accountEmail = "het@gmail.com"
+var accoutPassword = 1234
+accountCity= "Botad"
+
+// accountId=224113 //Assignment to constant variable.
+accountEmail= "het9@gmail.com"
+accoutPassword=1234
+accountCity="Ahmedabad"
+
+let accountState;
+
+console.log(accountId);
+console.table([accountEmail, accoutPassword, accountCity ,accountState])
+
+/*
+do not use var 
+because of issues in functional scope and block scope
+*/
